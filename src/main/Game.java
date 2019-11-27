e89f1cca9463b920de82816fccfcab7782e088fd
Zach Weber <50677289+zweber22@users.noreply.github.com>
2019-11-27 11:30:53 -0500

    Updated the Game Class
    Reworked the game class to reflect the changes made in the Gui folder to the UI and Visible classes

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,154 +1,89 @@
-import Gui.GuiScreen;
+// ---------------------------------------------------------------------------
+// File name: Game.java
+// Project name: RoyalGameOfUr
+// ---------------------------------------------------------------------------
+// Creator’s name and email: Zachary Weber, weberz@etsu.edu
+// Course-Section: CSCI 1260
+//	Creation Date:	11-24-19
+// Date of Last Modification:	11-26-19
+// ---------------------------------------------------------------------------
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import javax.swing.JPanel;
 
-//This class manages the game itself and all aspects of it. It is called by the Start class to begin the game
-public class Game extends JPanel implements Runnable, MouseListener, MouseMotionListener
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ * Class Name: Game <br>
+ * Class Purpose: This class calls the UI and visibility methods to display everything to the game screen <br>
+ *
+ * <hr>
+ * Date created: 11-24-19 <br>
+ * Date last modified: 11-26-19
+ * @author Zachary Weber
+ */
+
+public class Game
 {
+    ChoiceHandler mHandler = new ChoiceHandler();
+    UI ui = new UI();
+    Visibility v = new Visibility(ui);
 
-    private static final long serialVersionUID = 1L;
-    public static final int WIDTH = 1280; //Sets the width of the Game
-    public static final int HEIGHT = 720; //Sets the height of the game
-    public static final Font main = new Font("Verdana", Font.PLAIN, 28); //Sets the font and font size
-    private Thread game;
-    private boolean running;
-    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 
-    private long startTime;
-    private long elapsed;
-    private boolean set;
-
-    private GuiScreen screen;
-
-    //Constructor for the Game class
-    public Game() {
-        setFocusable(true);
-        setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        addMouseListener(this);
-        addMouseMotionListener(this);
-
-        screen = GuiScreen.getInstance();
+    public static void main(String[] args)
+    {
+        new Game();
     }
+    /**
+     * Method Name: game <br>
+     * Method Purpose: To create an instance of the game <br>
+     *
+     * <hr>
+     * Date created: 11-24-19 <br>
+     * Date last modified: 11-26-19 <br>
+     *
+     * <hr>
+     * Notes on specifications, special algorithms, and assumptions:
+     *   notes go here name
+     *
+     * <hr>
+     *   @param
+     *   @return void
+     */
 
-    private void update() {
-        screen.update();
+    public Game()
+    {
+        ui.createUI(mHandler);
+        v.showMainMenu();
     }
+    /**
+     * Method Name: ChoiceHandler <br>
+     * Method Purpose: Implements an action listener to manage a switch which holds different choices for the user <br>
+     *
+     * <hr>
+     * Date created: 11-24-19 <br>
+     * Date last modified: 11-26-19 <br>
+     *
+     * <hr>
+     * Notes on specifications, special algorithms, and assumptions:
+     *   notes go here name
+     *
+     * <hr>
+     *   @param
+     *   @return
+     */
+    public class ChoiceHandler implements ActionListener
+    {
 
-    private void render() {
-        Graphics2D g = (Graphics2D) image.getGraphics();
-        g.setColor(Color.white);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-        screen.render(g);
-        g.dispose();
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String choice = e.getActionCommand();
 
-        Graphics2D g2d = (Graphics2D) getGraphics();
-        g2d.drawImage(image, 0, 0, null);
-        g2d.dispose();
-
-    }
-
-    @Override
-    public void run() {
-        int fps = 0, updates = 0;
-        long fpsTimer = System.currentTimeMillis();
-        double nsPerUpdate = 100000000.0 / 60;
-
-        //last update time in nanoseconds
-        double then = System.nanoTime();
-        double unprocessed = 0;
-
-        while(running) {
-
-            boolean shouldRender = false;
-            double now = System.nanoTime();
-            unprocessed+=(now-then) / nsPerUpdate;
-            then = now;
-
-            //Update queue
-            while(unprocessed >= 1) {
-                updates++;
-                update();
-                unprocessed--;
-                shouldRender = true;
-            }
-
-            //rendering
-            if(shouldRender) {
-                fps++;
-                render();
-                shouldRender = false;
-            }
-            else {
-                try {
-                    Thread.sleep(1);
-                }catch(Exception e) {
-                    e.printStackTrace();
-                }
+            switch(choice)
+            {
+                case "Play": v.mainMenuToBoard(); break;
+                case "Quit": break; //To be added
+                //case "roll": break; //To be added
+               // case "pass": break; //To be added
             }
         }
-        // FPS Timer
-        if(System.currentTimeMillis() - fpsTimer > 1000) {
-            System.out.printf("%d fps %d updates, fps, updates");
-            System.out.println();
-            fps = 0;
-            updates = 0;
-            fpsTimer += 1000;
-        }
-    }
-
-    public synchronized void start() {
-        if(running) return;
-        running = true;
-        game = new Thread(this,"game");
-        game.start();
-    }
-
-    public synchronized void stop() {
-        if(!running) return;
-        running = false;
-        System.exit(0);
-    }
-
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e)
-    {
-        screen.mousePressed(e);
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e)
-    {
-        screen.mouseReleased(e);
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e)
-    {
-        screen.mouseDragged(e);
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e)
-    {
-        screen.mouseMoved(e);
     }
 }
